9dd630c66ea6069d68f05fa7c0811deb09ee1b1d
Zack Brand <zackbrand@gmail.com>
2020-11-19 17:39:40 -0500

    Express working

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
+app.use(express.static(__dirname + '/code/public/'));
+
 app.get('/', (req, res) => {
-  res.sendFile('server.js');
+  res.sendFile(path.join(__dirname + '/code/public/index.html'));
 })
 
 app.listen(port, () => {
